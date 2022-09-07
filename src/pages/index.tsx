de6da6684637bb8397dcf96db8fdaf2b2aca8cd2
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-09-07 04:18:42 -0300

    feat: Estrutura home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,37 @@
-import { styled } from "../styles";
+import FutureImage from "next/future/image";
 
-const Button = styled("button", {
-  backgroundColor: "$green500",
-  borderRadius: 4,
-  border: 0,
-  padding: "4px 8px",
+import tShirt1 from "../assets/shirt-1.png";
+import tShirt2 from "../assets/shirt-2.png";
+import tShirt3 from "../assets/shirt-3.png";
+import tShirt4 from "../assets/shirt-4.png";
 
-  span: {
-    fontWeight: "bold",
-  },
-
-  "&:hover": {
-    filter: "brightness(0.8)",
-  },
-});
+import { HomeContainer, Product } from "../styles/pages/home";
 
 export default function Home() {
+  const products = [
+    tShirt1,
+    tShirt2,
+    // tShirt3,
+    // tShirt4,
+  ];
   return (
-    <div>
-      <Button>
-        Enviar <span>Stitche</span>
-      </Button>
-    </div>
+    <>
+      <HomeContainer>
+        {products.map((product) => (
+          <Product key={product.src}>
+            <FutureImage
+              src={product}
+              width={520}
+              height={480}
+              alt="camiseta"
+            />
+            <footer>
+              <strong>Camiseta X</strong>
+              <span>R$ 79,90</span>
+            </footer>
+          </Product>
+        ))}
+      </HomeContainer>
+    </>
   );
 }
---
+++ src/styles/pages/home.ts
@@ -0,0 +1,63 @@
+import { styled } from "..";
+
+export const HomeContainer = styled("main", {
+  display: "flex",
+  gap: "3rem",
+  width: "100vw",
+  maxWidth: "calc(100vw - ((100vw - 1180px) / 2))",
+  marginLeft: "auto",
+  minHeight: 656,
+});
+
+export const Product = styled("a", {
+  background: "linear-gradient(180deg, #1EA483 0%, #7465D4 100%)",
+  borderRadius: 8,
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  position: "relative",
+  cursor: "pointer",
+  overflow: "hidden",
+
+  img: {
+    objectFit: "cover",
+  },
+
+  footer: {
+    position: "absolute",
+    left: "0.25rem",
+    right: "0.25rem",
+    bottom: "0.25rem",
+
+    transform: "translateY(110%)",
+    opacity: 0,
+    transition: "all 0.2s ease-in-out",
+
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+
+    padding: "20px 32px 20px 20px",
+    gap: "8px",
+    background: "rgba(32, 32, 36, 0.9)",
+    borderRadius: "6px",
+
+    strong: {
+      fontSize: "$lg",
+    },
+
+    span: {
+      fontSize: "$xl",
+      FontWeight: "bold",
+      color: "$green500",
+    },
+  },
+
+  "&:hover": {
+    footer: {
+      transform: "translateY(0)",
+      opacity: 1,
+    },
+  },
+});
