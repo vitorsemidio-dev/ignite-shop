f3530e851f84ebc0c38b074f7730adb3e9fc0e17
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-09-07 02:38:54 -0300

    chore: Configuração biblioteca de estilos `Stitches`

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
+import { styled } from "../styles";
+
+const Button = styled("button", {
+  backgroundColor: "$rocketseat",
+  borderRadius: 4,
+  border: 0,
+  padding: "4px 8px",
+
+  span: {
+    fontWeight: "bold",
+  },
+
+  "&:hover": {
+    filter: "brightness(0.8)",
+  },
+});
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello World ts</h1>
+      <Button>
+        Enviar <span>Stitche</span>
+      </Button>
     </div>
   );
 }
---
+++ src/styles/index.ts
@@ -0,0 +1,18 @@
+import { createStitches } from "@stitches/react";
+
+export const {
+  config,
+  createTheme,
+  css,
+  getCssText,
+  globalCss,
+  keyframes,
+  theme,
+  styled,
+} = createStitches({
+  theme: {
+    colors: {
+      rocketseat: "#8257e6",
+    },
+  },
+});
