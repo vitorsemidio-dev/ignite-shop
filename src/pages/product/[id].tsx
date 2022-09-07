21de97e9bfaada0d688d24549af1f42d2044894e
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-09-07 16:58:03 -0300

    feat: Geração estática página cada produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,33 +1,43 @@
+import { GetStaticPaths, GetStaticProps } from "next";
 import FutureImage from "next/future/image";
 import { useRouter } from "next/router";
+import Stripe from "stripe";
+
+import { stripe } from "../../lib/stripe";
 import {
   ImageContainer,
   ProductContainer,
   ProductDetails,
 } from "../../styles/pages/product";
+import { formatCurrencyBRL } from "../../utils/number-format.util";
 
-import tShirtImg from "../../assets/shirt-1.png";
+type ProductType = {
+  id: string;
+  name: string;
+  imageUrl: string;
+  price: string;
+  description: string;
+};
 
-export default function ProductItem() {
-  const { query } = useRouter();
-  const { id } = query;
+interface ProductItemProps {
+  product: ProductType;
+}
+
+export default function ProductItem({ product }: ProductItemProps) {
+  const { isFallback } = useRouter();
+
   return (
     <>
       <ProductContainer>
         <ImageContainer>
-          <FutureImage src={tShirtImg} />
+          <FutureImage src={product.imageUrl} width={520} height={480} />
         </ImageContainer>
 
         <ProductDetails>
-          <h1>Camiseta X</h1>
-          <span>R$ 79,90</span>
+          <h1>{product.name}</h1>
+          <span>{product.price}</span>
 
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Perferendis dolorem laudantium voluptatibus, ullam similique,
-            dolorum optio incidunt ducimus sit quam corrupti ut totam possimus
-            architecto numquam aliquam. Eos, sint explicabo.
-          </p>
+          <p>{product.description}</p>
 
           <button>Comprar Agora</button>
         </ProductDetails>
@@ -35,3 +45,40 @@ export default function ProductItem() {
     </>
   );
 }
+
+type ParamsType = {
+  id: string;
+};
+
+export const getStaticProps: GetStaticProps<
+  ProductItemProps,
+  ParamsType
+> = async ({ params }) => {
+  const productId = params.id;
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ["default_price"],
+  });
+
+  const price = product.default_price as Stripe.Price;
+  const productProp = {
+    id: product.id,
+    name: product.name,
+    imageUrl: product.images[0],
+    price: formatCurrencyBRL(price.unit_amount / 100),
+    description: product.description,
+  };
+
+  return {
+    props: {
+      product: productProp,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [{ params: { id: "prod_MO5Pdc4m3rNLi3" } }],
+    fallback: true,
+  };
+};
