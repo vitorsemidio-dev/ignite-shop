7dd1cc509905f04cef355e795a726fd803b7e215
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-09-07 16:09:06 -0300

    feat: Estrutura visual página produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,37 @@
-import { useRouter } from "../../../node_modules/next/router";
+import FutureImage from "next/future/image";
+import { useRouter } from "next/router";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "../../styles/pages/product";
+
+import tShirtImg from "../../assets/shirt-1.png";
 
 export default function ProductItem() {
-  const { query } = useRouter() as any;
+  const { query } = useRouter();
   const { id } = query;
-  return <h1>Product Item with id: {id}</h1>;
+  return (
+    <>
+      <ProductContainer>
+        <ImageContainer>
+          <FutureImage src={tShirtImg} />
+        </ImageContainer>
+
+        <ProductDetails>
+          <h1>Camiseta X</h1>
+          <span>R$ 79,90</span>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Perferendis dolorem laudantium voluptatibus, ullam similique,
+            dolorum optio incidunt ducimus sit quam corrupti ut totam possimus
+            architecto numquam aliquam. Eos, sint explicabo.
+          </p>
+
+          <button>Comprar Agora</button>
+        </ProductDetails>
+      </ProductContainer>
+    </>
+  );
 }
